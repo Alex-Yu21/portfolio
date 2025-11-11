8deb572eaa0bd5c7fa594d96bc5c96c25b158ac6
Alex-Yu21 <yualex2112@gmail.com>
2025-11-11 15:29:16 +0100

    style: responsive text sizes extention

---
+++ lib/theme/extensions.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/theme/app_text_styles.dart';
+
+enum DeviceTipe { mobile, tablet, desktop }
+
+extension StyledContext on BuildContext {
+  MediaQueryData get mg => MediaQuery.of(this);
+  double get wigth => mg.size.width;
+  double get heigth => mg.size.height;
+
+  DeviceTipe get deviceTipe {
+    if (wigth < 600) {
+      return DeviceTipe.mobile;
+    } else if (wigth < 900) {
+      return DeviceTipe.tablet;
+    } else {
+      return DeviceTipe.mobile;
+    }
+  }
+
+  bool get isMobile => deviceTipe == DeviceTipe.mobile;
+  bool get isTablet => deviceTipe == DeviceTipe.tablet;
+  bool get isDesktop => deviceTipe == DeviceTipe.desktop;
+
+  AppTextStyles get textStyles {
+    switch (deviceTipe) {
+      case DeviceTipe.mobile:
+      case DeviceTipe.tablet:
+        return SmallTextStyles();
+      case DeviceTipe.desktop:
+        return LargeTextStyles();
+    }
+  }
+}
