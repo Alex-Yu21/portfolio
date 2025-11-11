2087e8d9c05eca9a3f09fab7be1301818032bdcd
Alex-Yu21 <yualex2112@gmail.com>
2025-11-11 16:16:00 +0100

    style: colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/presentation/pages/home_page.dart';
+import 'package:portfolio/theme/app_themes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: AppThemes.dark,
+      home: HomePage(),
     );
   }
 }
